bde995940f4197068405de5fb989bbe8630c2338
mcquin <mcquin@broadinstitute.org>
2016-09-19 13:31:36 -0400

    Test with single-channel 2d, multi-channel 2d, and single-channel 3d images

--- tests/modules/test_dilation.py
+++ tests/modules/test_dilation.py
@@ -1,4 +1,5 @@
 import cellprofiler.modules.dilation
+import cellprofiler.setting
 import numpy.testing
 import skimage.morphology
 
@@ -10,14 +11,15 @@ def test_run(image, module, image_set, workspace):
 
     module.y_name.value = "dilation"
 
-    disk = skimage.morphology.disk(1)
-
-    module.structuring_element.value = disk
+    if image.dimensions is 3 or image.multichannel:
+        module.structuring_element = cellprofiler.setting.StructuringElement(shape="ball", size=1)
+    else:
+        module.structuring_element = cellprofiler.setting.StructuringElement(shape="disk", size=1)
 
     module.run(workspace)
 
     actual = image_set.get_image("dilation")
 
-    desired = skimage.morphology.dilation(image.pixel_data, disk)
+    desired = skimage.morphology.dilation(image.pixel_data, module.structuring_element.value)
 
     numpy.testing.assert_array_equal(actual.pixel_data, desired)
