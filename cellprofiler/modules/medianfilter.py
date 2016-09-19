4bee9d3cbb7fa806627289b28fd3c91d593a5938
mcquin <mcquin@broadinstitute.org>
2016-09-19 14:33:17 -0400

    unicode options, convert 3d structuring element to 2d

--- cellprofiler/modules/medianfilter.py
+++ cellprofiler/modules/medianfilter.py
@@ -20,50 +20,34 @@ class MedianFilter(cellprofiler.module.Module):
 
     def create_settings(self):
         self.x_name = cellprofiler.setting.ImageNameSubscriber(
-            "Input"
+            u"Input"
         )
 
         self.y_name = cellprofiler.setting.ImageNameProvider(
-            "Output",
-            "OutputImage"
+            u"Output",
+            u"OutputImage"
         )
 
-        self.structuring_element = cellprofiler.setting.Choice(
-            "Structuing element",
-            [
-                "Ball"
-            ]
-        )
-
-        self.radius = cellprofiler.setting.Integer(
-            "Radius",
-            1
-        )
+        self.structuring_element = cellprofiler.setting.StructuringElement()
 
     def settings(self):
         return [
             self.x_name,
             self.y_name,
-            self.structuring_element,
-            self.radius
+            self.structuring_element
         ]
 
     def visible_settings(self):
         return [
             self.x_name,
             self.y_name,
-            self.structuring_element,
-            self.radius
+            self.structuring_element
         ]
 
     def run(self, workspace):
         x_name = self.x_name.value
 
         y_name = self.y_name.value
-
-        radius = self.radius.value
-
-        structuring_element = self.structuring_element.value
 
         images = workspace.image_set
 
@@ -73,12 +57,13 @@ class MedianFilter(cellprofiler.module.Module):
 
         x_data = skimage.img_as_uint(x_data)
 
-        disk = skimage.morphology.disk(radius)
+        #TODO: use actual structuring_element when 3D is available for skimage.filters
+        selem = self.__structuring_element()
 
         y_data = numpy.zeros_like(x_data)
 
         for plane, image in enumerate(x_data):
-            y_data[plane] = skimage.filters.rank.median(image, disk)
+            y_data[plane] = skimage.filters.rank.median(image, selem)
 
         y_data = skimage.exposure.rescale_intensity(y_data * 1.0)
 
@@ -100,3 +85,15 @@ class MedianFilter(cellprofiler.module.Module):
         figure.gridshow(0, 0, workspace.display_data.x_data)
 
         figure.gridshow(0, 1, workspace.display_data.y_data)
+
+    def __structuring_element(self):
+        if self.structuring_element.shape == "ball":
+            return skimage.morphology.disk(self.structuring_element.size)
+
+        if self.structuring_element.shape == "cube":
+            return skimage.morphology.square(self.structuring_element.size)
+
+        if self.structuring_element.shape == "octahedron":
+            return skimage.morphology.diamond(self.structuring_element.size)
+
+        return self.structuring_element.value
